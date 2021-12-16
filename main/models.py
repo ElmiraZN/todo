f91f3438a083747020a0a87f30f6559bac7d488f
ElmiraZN <elmirazakirova@yahoo.com>
2021-12-16 21:07:41 +0100

    added -manager- superuser and made migrations of ToDo to ../admin

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+class ToDo(models.Model):
+    text = models.CharField(default="Task", max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
