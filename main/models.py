0b82b5174f20fb37876df689314260af6f1d1f9a
ElmiraZN <elmirazakirova@yahoo.com>
2021-12-17 14:26:44 +0100

    added new ToMeet model and configured it

--- main/models.py
+++ main/models.py
@@ -6,3 +6,10 @@ class ToDo(models.Model):
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=12)
+    phone_number = models.IntegerField()
+    date_of_meeting = models.DateField(auto_now=False)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
